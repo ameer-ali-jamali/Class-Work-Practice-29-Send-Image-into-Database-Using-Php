69cfb8db6cafff987cedde82f9c71ff1c1a83486
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-12-11 00:52:01 +0500

    .

---
+++ helper.php
@@ -0,0 +1,54 @@
+<?php
+include_once "conn.php";
+
+// Signup Function With Image ...
+function signup($name, $email, $pass, $img)
+{
+    global $conn;
+    if ($name && $email && $pass && $img != "") {
+        $folder = "uploads/";
+        $target = $folder . basename($_FILES['img']['name']);
+        move_uploaded_file($img, $target);
+        $sql = "INSERT INTO `tab`(`name`, `email`, `pass`, `img`) VALUES ('$name','$email','$pass','$target')";
+        try {
+            $result = mysqli_query($conn, $sql);
+        } catch (Exception $ex) {
+            if ($ex) {
+                return die("<script>
+                alert('This Email Already Exists Please Try Other Email');
+                window.location = 'index.php';
+                </script>");
+            }
+        }
+        return $result;
+    } else {
+        return die("<script>
+    alert('Please Fill Complete Registartion Form');
+    window.location = 'index.php';
+    </script>");
+    }
+}
+
+if (isset($_POST['submit'])) {
+    $registration = signup(
+        $_POST['name'],
+        $_POST['email'],
+        $_POST['pass'],
+        $_FILES['img']['tmp_name']
+    );
+    if ($registration == true) {
+        die("<script>
+            alert('Your Data Submited Successfully');
+            window.location = 'index.php';
+            </script>");
+    } else {
+        die("<script>
+            alert('Please Fill Registration Form');
+            window.location = 'index.php';
+            </script>");
+    }
+}
+
+?>
+<?php
+?>
